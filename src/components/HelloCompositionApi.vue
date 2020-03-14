32b491e7bfcaad1e2f6be2d53cebb37143445af6
Alvaro Tanarro <tanarro+adblog@gmail.com>
2020-03-14 19:14:47 +0100

    Lifecycle Hooks

--- src/components/HelloCompositionApi.vue
+++ src/components/HelloCompositionApi.vue
@@ -1,9 +1,19 @@
 <script>
+// import {onBeforeMount, onMounted} from 'vue'; This will work on vue3
 import useEventSpace from "../use/event-space";
 import useMapping from "../use/mapping";
 
 export default {
+    beforeMount() {
+        console.log('onBeforeMount');
+    },
+    mounted() {
+        console.log('onMounted');
+    },
     setup(props, context) {
+        // This will work on vue3
+        // onBeforeMount(() => { ... })
+        // onMounted(() => { ... })
         console.log(props, context);
         return { ...useEventSpace(), ...useMapping };
     }
