e23f4aee8d9843a7ae23075e29d10bf879ad707d
Alvaro Tanarro <tanarro+adblog@gmail.com>
2020-03-14 18:57:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloCompositionApi />
+  </div>
+</template>
+
+<script>
+import HelloCompositionApi from './components/HelloCompositionApi.vue'
+
+export default {
+  name: 'App',
+  components: {
+      HelloCompositionApi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloCompositionApi.vue
@@ -0,0 +1,34 @@
+<script>
+    import { ref, computed } from "@vue/composition-api";
+    export default {
+        setup(props, context) {
+            console.log(props, context);
+            const capacity = ref(3);
+            const attending = ref(['a person', 'another person', 'a third one']);
+            function increaseCapacity() {
+                capacity.value++;
+            }
+            const spacesLeft = computed(() => {
+                return capacity.value - attending.value.length;
+            });
+            return { capacity, increaseCapacity, attending, spacesLeft };
+        }
+    };
+</script>
+
+<template>
+    <div>
+        <h1>My Event</h1>
+        <p>Spaces left: {{ spacesLeft }}</p>
+        <p>Capacity: {{ capacity }}</p>
+        <button @click="increaseCapacity()">Increase Capacity</button>
+        <h2>Attending</h2>
+        <ul>
+            <ol
+                :key="attendee"
+                v-for="attendee in attending"
+            >{{ attendee }}</ol>
+        </ul>
+
+    </div>
+</template>
